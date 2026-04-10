e15e6cb81545c08a2ad4d87a4d730576a316991d
esmaeil taheri <esi.taheri@yahoo.com>
2026-04-10 12:39:34 +0330

    Implement CustomUser model and admin section

--- src/apps/accounts/admin.py
+++ src/apps/accounts/admin.py
@@ -7,4 +7,51 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
 
-    list_display = ('id', 'username', 'email', 'full_name', 'is_staff')
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide', ),
+                'fields': (
+                    'username', 'usable_password', 'password1', 'password2',
+                    'phone_number', 'national_id', 'is_suspended', 'otp',
+                    'otp_expires_timestamp', 'last_login_timestamp', 'last_ip_address',
+                    'is_2fa_enabled', 'requires_2fa', 'totp_secret',
+                )
+            }
+        ),
+    )
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff',
+         'is_superuser', 'groups', 'user_permissions')}),
+        ('Authentication', {
+            'fields': (
+                'registration',
+                'shamsi_last_login', 'shamsi_otp_expires', 'last_ip_address',
+                'is_suspended', 'phone_number_ownership',
+                'otp', 'otp_expires_timestamp', 'last_login_timestamp',
+                'is_2fa_enabled', 'requires_2fa', 'totp_secret',
+
+            ) 
+        }),
+        ('Personal Info', {'fields': (
+            'first_name', 'last_name', 'email', 'phone_number', 'national_id')}),
+    )
+
+    readonly_fields = [
+        'registration',
+        'shamsi_last_login',
+        'shamsi_otp_expires',
+        'last_ip_address',
+        'totp_secret'
+    ]
+
+    list_display_links = ['id', 'national_id']
+
+    list_display = ('id', '__str__', 'phone_number', 'national_id', 'is_2fa_enabled',
+                    'is_suspended', 'phone_number_ownership', 'last_ip_address', 'otp', 'shamsi_last_login', )
+    
+    search_fields = ['id', 'national_id', 'phone_number', 'first_name', 'last_name']
+    
--- src/apps/accounts/models/user.py
+++ src/apps/accounts/models/user.py
@@ -3,11 +3,56 @@ from django.contrib.auth.models import (
     AbstractUser, BaseUserManager
 )
 
+import pyotp
+import jdatetime
+
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True, max_length=255, blank=True)
+    phone_number = models.CharField(max_length=11, unique=True, verbose_name='Phone Number', default='-')
+    national_id = models.CharField(max_length=10, unique=True, verbose_name="National ID", default='-')
+
+    is_suspended = models.BooleanField(default=False, verbose_name='Is Suspended')
+    phone_number_ownership = models.BooleanField(default=False, verbose_name='Mobile Ownership')
+
+    otp = models.CharField(max_length=6, default='-', verbose_name='OTP')
+    otp_expires_timestamp = models.IntegerField(default=0, verbose_name='OTP Expires')
+
+    last_login_timestamp = models.IntegerField(default=0, verbose_name='Last Login')
+    last_ip_address = models.GenericIPAddressField(default='0.0.0.0', verbose_name='Last Ip Address')
+
+    is_2fa_enabled = models.BooleanField(default=False, verbose_name='Is 2FA Enabled')
+    requires_2fa = models.BooleanField(default=False, verbose_name='Is 2FA Enabled')
+    totp_secret = models.CharField(max_length=32, verbose_name='2FA Secret', blank=True)
+
+    created_timestamp = models.IntegerField(default=0, verbose_name='Created On')
+
+    class Meta:
+        unique_together = [['phone_number', 'national_id', ]]
 
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
+    
+    def get_totp_uri(self):
+        return pyotp.TOTP(self.totp_secret).provisioning_uri(
+            name=self.national_id,
+            issuer_name="Exchange"
+        )
+    
+    def verify_totp(self, code):
+        totp = pyotp.TOTP(self.totp_secret)
+        return totp.verify(code)
+    
+    @property
+    def shamsi_last_login(self):
+        return jdatetime.datetime.fromtimestamp(int(self.last_login_timestamp))
+    
+    @property
+    def shamsi_otp_expires(self):
+        return jdatetime.datetime.fromtimestamp(int(self.otp_expires_timestamp))
+    
+    @property
+    def registration(self):
+        return jdatetime.datetime.fromtimestamp(int(self.created_timestamp))
 
     def __str__(self):
-        return self.username
+        return f'{self.first_name} {self.last_name}'
+        
