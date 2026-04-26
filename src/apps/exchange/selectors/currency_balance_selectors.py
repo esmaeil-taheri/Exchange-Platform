1a898ff738c683e573d4171aebf43f86e1a6b3df
esmaeil taheri <esi.taheri@yahoo.com>
2026-04-26 21:15:26 +0330

    Lock the currency active balance

--- src/apps/exchange/selectors/currency_balance_selectors.py
+++ src/apps/exchange/selectors/currency_balance_selectors.py
@@ -6,4 +6,8 @@ class CurrencyBalanceSelector:
     @staticmethod
     def get_currency_balance_by_symbol(symbol: str):
         return CurrencyBalance.objects.get(currency__symbol=symbol)
-
+    
+    @staticmethod   
+    def get_active_balance_for_update_by_symbol(symbol: str):
+        return CurrencyBalance.objects.select_for_update().get(
+            currency__symbol=symbol).active_balance
--- src/apps/exchange/services/currency_balance_service.py
+++ src/apps/exchange/services/currency_balance_service.py
@@ -6,7 +6,6 @@ class CurrencyBalanceSerivce:
 
     @staticmethod
     def calculate_available_balance_for_update(symbol: str):
-        currency_balance = CurrencyBalanceSelector.get_currency_balance_by_symbol(symbol=symbol)
-        active_balance = currency_balance.active_balance
+        active_balance = CurrencyBalanceSelector.get_active_balance_for_update_by_symbol(symbol=symbol)
         pending_balance = TransactionSelector.get_pending_transactions_amount_for_update(symbol=symbol)
         return round(active_balance - pending_balance, 4)
